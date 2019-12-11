a56d8c14bbc1066e2bdb349de5d3b1cdcec283fc
Franz Dippl <franz.dippl@isb.bayern.de>
2019-12-11 15:33:53 +0100

    do not show visitationnotes marked as done

--- resources/views/PdfDemo.blade.php
+++ resources/views/PdfDemo.blade.php
@@ -18,9 +18,9 @@
 <body>
 <div style="text-align: right">
     <img src="/img/logo.png" style="width: 120px">
-  </div><\br>
-<p style="border-top: 1px solid #a42600"><span style="color:#a42600; font-size: 1em">{{ $project['name'] }} </span><span style="color:#a42600; font-size: 0.6em">- {{ $project['street'] }} {{ $project['housenumber'] }}, {{ $project['postcode'] }} {{ $project['city'] }}</span><br><span style="color:grey; font-size: 1.4em">{{ $visit['title'] }}, {{ $visit['date'] }}</span>
-</p>
+  </div>
+<p style="line-height: 0.5em; border-bottom: 1px solid #a42600;"><span style="color:#a42600; font-size: 1.2em">{{ $project['name'] }} </span><span style="color:#a42600; font-size: 0.8em">- {{ $project['street'] }} {{ $project['housenumber'] }}, {{ $project['postcode'] }} {{ $project['city'] }}</span></p>
+<span style="border-top: 1px solid #a42600;color:grey; font-size: 1.4em">{{ $visit['title'] }}, {{ $visit['date'] }}</span><br>
 <p style="font-size:0.7em"><b>Datum:</b> {{ $visit['date'] }}, {{ substr($visit['time'],0, 5) }}&nbsp;&nbsp;&nbsp;&nbsp;<b>Bauleiter:</b> {{ $responsible }}&nbsp;&nbsp;&nbsp;&nbsp;@if( $visit['weather'] != "")<b>Wetter:</b> @endif{{ $visit['weather'] }}</p>
 <p></p>
 <p style="font-size: 1em; color: #a42600; line-height: 0.9em">Anwesende</p>
