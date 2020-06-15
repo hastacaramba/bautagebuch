c6ba5b46c88b01c89dbb80d6812955762531ac42
Franz Dippl <franz.dippl@isb.bayern.de>
2020-06-15 15:41:09 +0200

    updated select2

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -19,7 +19,8 @@
     <link href="../../vendor/fontawesome-free/css/all.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="../../vendor/select2/dist/css/select2.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+
 
     <link href="../../vendor/bootstrap-table/dist/bootstrap-table.min.css" rel="stylesheet" type="text/css">
 
@@ -36,7 +37,7 @@
 
     <script src="../../vendor/jquery/jquery.min.js"></script>
 
-    <script src="../../vendor/select2/dist/js/select2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
 
 
 
