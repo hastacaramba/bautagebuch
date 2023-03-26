add94281c20ff012651fd1fef74c2e802e837ee7
hastacaramba <56657068+hastacaramba@users.noreply.github.com>
2023-03-26 17:05:06 +0200

    next try

--- resources/views/bauprojekte.blade.php
+++ resources/views/bauprojekte.blade.php
@@ -621,10 +621,8 @@
     }
      
     function numericSort(a, b, rowA, rowB) {  
-      //a = (int)a;
-      //b = (int)b;
-      a = +a.substring(1); // remove $
-      b = +b.substring(1);
+      a = (int)+a.substring(1); // remove $
+      b = (int)+b.substring(1);
       if (a > b) return 1;
       if (a < b) return -1;
       return 0;
