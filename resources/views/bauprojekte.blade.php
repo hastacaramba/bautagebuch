b0393d4bb59d2ed6a43719532e069463d4c9736a
hastacaramba <56657068+hastacaramba@users.noreply.github.com>
2023-03-27 16:42:36 +0200

    try

--- resources/views/bauprojekte.blade.php
+++ resources/views/bauprojekte.blade.php
@@ -448,6 +448,7 @@
         '<a class="delete" href="javascript:void(0)" title="Löschen">',
         '<button type="button" class="btn btn-default" style="color:#345589; border: none" ><i class="fas fa-trash"></i></button>',
         '</a>  '
+        '<a class="rotate" href="javascript:void(0)" title="Bild rotieren">',
         '<button type="button" class="btn btn-default" style="color:#345589; border: none" ><i class="fas fa-plus-circle"></i></button>',
         '</a>  '
       ].join('')
