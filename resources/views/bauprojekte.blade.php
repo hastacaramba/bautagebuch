a0cec8b36bc01e7e90934cba3212e20bfad69e59
hastacaramba <56657068+hastacaramba@users.noreply.github.com>
2023-03-27 17:12:55 +0200

    try

--- resources/views/bauprojekte.blade.php
+++ resources/views/bauprojekte.blade.php
@@ -529,7 +529,7 @@
       },
       'click .imgRotate': function (e, value, row, index) {
         $.ajax({
-        url: '/media/rotate/' + row.photo,
+        url: '/media/rotate/' + row.photo.val(),
         data: {
         },
         type: 'PATCH',
