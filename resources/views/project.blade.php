940128e97cc9ea827142d3bbcd39971c0f583452
hastacaramba <info@dippl-it-services.de>
2023-04-26 21:31:01 +0200

    test

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -128,7 +128,7 @@
                                     data-side-pagination="client"
                                     data-toggle="table"
                                     data-sortable="true"
-                                    data-url="/visitationnotesByProject/{{ $projectID }}"
+                                    data-url="/visitationnotesByProject/34"
                                     data-toolbar="#toolbar"
                                     data-search="true"
                                     data-show-columns="true"
