92e5e849bcef3a3a50dcab061c709803e131b86c
hastacaramba <info@dippl-it-services.de>
2023-04-26 22:12:54 +0200

    try

--- app/Http/Controllers/VisitationnoteController.php
+++ app/Http/Controllers/VisitationnoteController.php
@@ -51,8 +51,8 @@ class VisitationnoteController extends Controller
         //fetch the visitationnotes of the visit
         $visitationnotes = Visitationnote::where('visit_id', '=', $visits[$n]['id'])->get();
 
-        $allVisitationnotesOfVisit .= json_encode($visitationnotes[0]) 
-        
+        $allVisitationnotesOfVisit .= json_encode($visitationnotes[0]); 
+
         if ($n < sizeof($visits) - 1) {
             $allVisitationnotesOfVisit .= ",";  
         }
