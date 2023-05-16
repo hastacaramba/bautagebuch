cf0fa5c4e87a3a764238335cbbe7d8945204dcc1
hastacaramba <info@dippl-it-services.de>
2023-05-16 18:26:13 +0200

    try

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -1444,7 +1444,7 @@ function initTableConcernedMembers() {
             $("#modalEditVisitationnnote").modal('toggle');
             $("#visitationnoteDate").val(row.created_at.substring(0,row.created_at.length - 9));
             $("#visitationnoteNumber").val(row.number);
-            let deadline = newDate(row.deadline);
+            let deadline = new Date(row.deadline);
             let y = deadline.getFullYear;
             let m = deadline.getMonth;
             let d = deadline.getDate;
