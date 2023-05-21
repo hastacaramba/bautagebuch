5f42f71d39779bcd60eddbda5e57e5715fbfbd03
hastacaramba <info@dippl-it-services.de>
2023-05-21 14:26:05 +0200

    try

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -77,7 +77,7 @@ class ImageUploadController extends Controller
             $imageNames[] = $imageName;
 
         }
-        return "test";
+        return $request->file('images');
     }
 
     /**
