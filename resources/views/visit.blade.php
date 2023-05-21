cdf94c570e98e03e99f61fa5af4e838e27eeedbb
hastacaramba <info@dippl-it-services.de>
2023-05-21 17:20:50 +0200

    try

--- resources/views/visit.blade.php
+++ resources/views/visit.blade.php
@@ -2186,7 +2186,7 @@
 
             const test = ["file1", "file2"]; 
 
-            formData.append("newFiles[]", test);
+            formData.append("newFiles", test);
             
             formData.append('visitationnoteID', $("#visitationnoteID").val());
 
