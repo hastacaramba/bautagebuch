93e5da5bed37dd96d343830dd2907bc7bf02c8d0
hastacaramba <info@dippl-it-services.de>
2023-05-21 16:46:57 +0200

    try

--- resources/views/visit.blade.php
+++ resources/views/visit.blade.php
@@ -2180,7 +2180,7 @@
             myFiles = e.target.files;            
           
             // Attach files
-            formData.append('newImages[]', myFiles);
+            formData.append('newImages', myFiles);
 
             formData.append('visitationnoteID', $("#visitationnoteID").val());
 
