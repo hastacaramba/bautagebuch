e9b4ca56db80cc79b4f368b467f164132f9a4e92
hastacaramba <info@dippl-it-services.de>
2023-05-22 15:14:33 +0200

    date Formatted

--- resources/views/visit.blade.php
+++ resources/views/visit.blade.php
@@ -2138,7 +2138,23 @@
       $(document).ready(function () {
         
 
-        
+          var date = new Date($("#date").val());
+          var d = date.getDate();
+          var m = date.getMonth() + 1;
+          var h = date.getHours();
+          var min = date.getMinutes();
+          if((date.getMonth() + 1) < 10) {
+              m = "0" + m;
+          }
+          if(date.getDate() < 10) {
+              d = "0" + d;
+          }
+          var y = date.getFullYear().toString();
+
+
+          var output = d + "." + m + "." + y;    
+
+          $("#date").val(output);
         
 
 
