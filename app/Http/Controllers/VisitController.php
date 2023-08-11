fcefb6e8989d060ccbcf05d21fd46117a468056f
hastacaramba <info@dippl-it-services.de>
2023-08-11 08:18:05 +0200

    try

--- app/Http/Controllers/VisitController.php
+++ app/Http/Controllers/VisitController.php
@@ -381,5 +381,21 @@ class VisitController extends Controller
         return $pdfs;
     }
 
+         /**
+     * Update project attribute updated_at with the current timestamp.
+     *
+     * @param $id The id of the project
+     * @return Response
+     */
+    public function updateProjectUpdatedAt($projectID) {
+        $project = Project::where('id', '=', $projectID)->first();
+        $now = time();
+
+        if ($project != null) {
+            $project->updated_at = $now;
+            $project->save();
+        }
+    }
+
 
 }
