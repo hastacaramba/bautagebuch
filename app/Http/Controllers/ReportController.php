f1854ea6bb9cb06c45dc624e2a4e3260354f7836
hastacaramba <info@dippl-it-services.de>
2026-08-06 08:59:49 +0200

    reports try

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -89,7 +89,7 @@ class ReportController extends Controller {
 
         foreach ($reports as $report) {
             //if ($projectID == $report->visit['project_id']) {
-            //if ($projectID == $report->visit['project_id']) {
+            if ($projectID == $report->visit['project_id']) {
 
             $item = [
                 'id' => $report->id,
@@ -100,7 +100,7 @@ class ReportController extends Controller {
             ];
 
             $result[] = $item;
-            //}
+            }
         }
 
         return json_encode($result);
